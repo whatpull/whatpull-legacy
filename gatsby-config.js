860cabf9e2367e6e2aa77f0570a5b189434a2a67
whatpull <whatpull@gmail.com>
2021-03-27 21:17:26 +0900

    robots.txt 설정 적용

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,16 +8,6 @@ module.exports = {
     siteUrl: "https://www.whatpull.com"
   },
   plugins: [
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sitemap",
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "pages",
-        path: "./src/pages/",
-      },
-      __key: "pages",
-    },
     {
       resolve: `gatsby-plugin-s3`,
       options: {
@@ -36,6 +26,24 @@ module.exports = {
         icon: `src/images/icon.png`,
         cache_busting_mode: `name`
       },
+    },
+    "gatsby-plugin-sitemap",
+    {
+      resolve: 'gatsby-plugin-robots-txt',
+      options: {
+        host: 'https://www.whatpull.com',
+        sitemap: 'https://www.whatpull.com/sitemap.xml',
+        policy: [{ userAgent: '*', allow: '/' }]
+      }
+    },
+    "gatsby-plugin-react-helmet",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: "./src/pages/",
+      },
+      __key: "pages",
     },
   ],
 };
