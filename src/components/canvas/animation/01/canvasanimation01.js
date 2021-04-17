dd3533b6999c1104223f6d21eb54db5f6d7388de
whatpull <whatpull@gmail.com>
2021-04-17 18:07:52 +0900

    애니메이션 01 신 요소 개발

--- src/components/canvas/animation/01/canvasanimation01.js
+++ src/components/canvas/animation/01/canvasanimation01.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect, useRef } from 'react';
 import * as style from './canvasanimation01.module.css';
 
-export default function CanvasAnimation01() {
+export default function CanvasAnimation01({ ratio }) {
     const [context, setContext] = useState();
     const canvas = useRef();
+    const centerX = useRef(0);
+    const centerY = useRef(0);
 
     useEffect(() => {
         const clearContext = (paramX, paramY, paramWidth, paramHeight) => {
@@ -16,13 +18,79 @@ export default function CanvasAnimation01() {
             }
         }
 
+        const drawContext = () => {
+            context.strokeStyle = '#FFFFFF';
+            context.fillStyle = '#FFFFFF';
+
+            drawSceneTent();
+        }
+
+        const drawSceneTent = () => {
+            const drawTent = (fillColor, degree, moveX, moveY, width, height) => {
+                const startX = centerX.current;
+                const startY = centerY.current - (height / 2);
+                const line01X = startX + (((width / 2) / 2) / 2);
+                const line01Y = startY; 
+                const line02X = line01X + ((width / 2) / 2);
+                const line02Y = line01Y + 30;
+                const line03X = line02X + (((width / 2) / 2) / 2);
+                const line03Y = line02Y + (height - 30);
+                const line04X = line03X - width;
+                const line04Y = line03Y;
+                const line05X = line04X + (((width / 2) / 2) / 2);
+                const line05Y = line04Y - (height - 30);
+                const line06X = line05X + ((width / 2) / 2);
+                const line06Y = line05Y - 30;
+    
+                context.save();
+                context.beginPath();
+                context.translate(startX, startY);
+                context.rotate(degree * (Math.PI / 180));
+                context.translate(-startX + moveX, -startY + moveY);
+                context.moveTo(startX, startY);
+                context.lineTo(line01X, line01Y);
+                context.lineTo(line02X, line02Y);
+                context.lineTo(line03X, line03Y);
+                context.lineTo(line04X, line04Y);
+                context.lineTo(line05X, line05Y);
+                context.lineTo(line06X, line06Y);
+                context.closePath();
+                context.fillStyle = fillColor;
+                context.fill();
+                context.lineWidth = 2;
+                context.strokeStyle = '#121212';
+                // context.stroke();
+                context.restore();
+            }
+
+            context.save();
+            context.beginPath();
+            drawTent('rgba(205, 0, 0, 1)', 0, 0, -45, 320, 180);
+            context.globalCompositeOperation = 'color-dodge';
+            drawTent('rgba(255, 0, 0, 1)', -15, 46, 0, 200, 200);
+            drawTent('rgba(255, 0, 0, 1)', 15, -47, 0, 200, 200);
+            context.restore();
+        }
+
+        const initCanvas = () => {
+            if(canvas.current) {
+                context.canvas.width = canvas.current.clientWidth * ratio;
+                context.canvas.height = canvas.current.clientHeight * ratio;
+                centerX.current = context.canvas.width / 2;
+                centerY.current = context.canvas.height / 2;
+                clearContext();
+                drawContext();
+                context.imageSmoothingEnabled = true;
+            }
+        }
+
         const initialize = () => {
             if(canvas) {
                 if(typeof context === "undefined") {
                     setContext(canvas.current.getContext("2d"));
                 }
                 if(context) {
-
+                    initCanvas();
                 }
             }
         }
@@ -33,7 +101,7 @@ export default function CanvasAnimation01() {
                 clearContext();
             }
         }
-    }, [context])
+    }, [context, ratio])
 
     return (
         <canvas
--- src/components/content/animation/01/animation01.js
+++ src/components/content/animation/01/animation01.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import * as style from './animation01.module.css';
 import CanvasAnimation01 from '../../../canvas/animation/01/canvasanimation01';
 
 export default function Animation01() {
+    const ratio = useRef(1.5);
+
     return (
         <div
             className={style.animation01__canvasWrap}>
-            <CanvasAnimation01 />
+            <CanvasAnimation01 ratio={ratio.current} />
         </div>
     )
 }
