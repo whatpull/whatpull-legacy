4eec2780e9a07dc02de445cd56e469fb2109a51f
whatpull <whatpull@gmail.com>
2021-04-19 16:40:15 +0900

    텐트 지지대, 문 추가 개발

--- src/components/canvas/animation/01/canvasanimation01.js
+++ src/components/canvas/animation/01/canvasanimation01.js
@@ -33,11 +33,11 @@ export default function CanvasAnimation01({ ratio }) {
                 const line01Y = startY; 
                 const line02X = line01X + ((width / 2) / 2);
                 const line02Y = line01Y + 30;
-                const line03X = line02X + (((width / 2) / 2) / 2);
+                const line03X = line02X + (((width / 2) / 2) / 2) + 15;
                 const line03Y = line02Y + (height - 30);
                 const line04X = line03X - width;
                 const line04Y = line03Y;
-                const line05X = line04X + (((width / 2) / 2) / 2);
+                const line05X = line04X + (((width / 2) / 2) / 2) + 15;
                 const line05Y = line04Y - (height - 30);
                 const line06X = line05X + ((width / 2) / 2);
                 const line06Y = line05Y - 30;
@@ -47,7 +47,6 @@ export default function CanvasAnimation01({ ratio }) {
                 context.translate(startX, startY);
                 context.rotate(degree * (Math.PI / 180));
                 context.translate(-startX + moveX, -startY + moveY);
-                context.moveTo(startX, startY);
                 context.lineTo(line01X, line01Y);
                 context.lineTo(line02X, line02Y);
                 context.lineTo(line03X, line03Y);
@@ -63,12 +62,89 @@ export default function CanvasAnimation01({ ratio }) {
                 context.restore();
             }
 
+            const drawDoor = (fillColor, degree, moveX, moveY, radius) => {
+                const startX = centerX.current;
+                const startY = centerY.current;
+                const line01X = startX + radius;
+                const line01Y = startY;
+                const line02X = line01X;
+                const line02Y = startY + radius;
+                const line03X = line02X - (2 * radius);
+                const line03Y = line02Y;
+                const line04X = line03X;
+                const line04Y = line03Y - radius;
+                const line05X = line04X + radius;
+                const line05Y = line04Y;
+
+                context.save();
+                context.beginPath();
+                context.translate(startX, startY);
+                context.rotate(degree * (Math.PI / 180));
+                context.translate(-startX + moveX, -startY + moveY);
+                context.moveTo(startX, startY);
+                context.arc(startX, startY, radius, Math.PI * 1, Math.PI * 2, false);
+                context.closePath();
+                context.lineTo(line01X, line01Y);
+                context.lineTo(line02X, line02Y);
+                context.lineTo(line03X, line03Y);
+                context.lineTo(line04X, line04Y);
+                context.lineTo(line05X, line05Y);
+                context.fillStyle = fillColor;
+                context.fill();
+                context.strokeStyle = '#121212';
+                // context.stroke();
+                context.restore();
+            }
+            // 텐트 지지대
+            drawTent('#121212', 0, 0, -38, 363, 180);
+            drawTent('#673AB7', 0, 0, -35, 360, 180);
+            // 텐트 뒷면
+            drawTent('rgba(205, 0, 0, 1)', 0, 0, -25, 390, 170);
+            // 텐트 오른쪽
+            context.save()
+            context.beginPath();
+            drawTent('rgba(235, 0, 0, 0.95)', 13, -53, -3, 200, 200);
+            context.globalCompositeOperation = 'source-atop';
+            drawDoor('#121212', 5, -70, 35, 70);
+            drawDoor('rgba(255, 0, 0, 1)', 5, -70, 35, 66);
+            context.restore();
+            // 텐트 왼쪽
+            context.save()
+            context.beginPath();
+            drawTent('rgba(225, 0, 0, 0.95)', -13, 53, 4, 200, 200);
+            context.globalCompositeOperation = 'source-atop';
+            drawDoor('#121212', -5, 100, 37, 70);
+            drawDoor('rgba(255, 0, 0, 1)', -5, 100, 37, 66);
+            context.restore();
+            // 텐트 정면 지지대
+            drawRoundedRectangle(centerX.current + 12.5, centerY.current - 138, 3, 245, 0, 'fill', '#121212');
+        }
+
+        /** @param {*} type (타입 : line(선), fill(채우기)) */
+        const drawRoundedRectangle = (x, y, width, height, radius, type, color, alpha) => {
+            const topRight = typeof radius === 'number' ? radius : radius.topRight;
+            const bottomRight = typeof radius === 'number' ? radius : radius.bottomRight;
+            const bottomLeft = typeof radius === 'number' ? radius : radius.bottomLeft;
+            const topLeft = typeof radius === 'number' ? radius : radius.topLeft;
             context.save();
             context.beginPath();
-            drawTent('rgba(205, 0, 0, 1)', 0, 0, -45, 320, 180);
-            context.globalCompositeOperation = 'color-dodge';
-            drawTent('rgba(255, 0, 0, 1)', -15, 46, 0, 200, 200);
-            drawTent('rgba(255, 0, 0, 1)', 15, -47, 0, 200, 200);
+            context.moveTo(x + (topRight * 2), y);
+            context.arcTo(x + width, y, x + width, y + (topRight * 2), topRight); // top, right
+            context.lineTo(x + width, y + (topRight * 2));
+            context.arcTo(x + width, y + height, x + width - (bottomRight * 2), y + height, bottomRight); // bottom, right
+            context.lineTo(x + width - (bottomRight * 2), y + height);
+            context.arcTo(x, y + height, x, y + height - (bottomLeft * 2), bottomLeft); // bottom, left
+            context.lineTo(x, y + height - (bottomLeft * 2));
+            context.arcTo(x, y, x + (topLeft * 2), y, topLeft); // top, left
+            context.lineTo(x + (topLeft * 2), y);
+            if(typeof alpha === "number") context.globalAlpha = alpha;
+            if(type === 'fill') {
+                context.fillStyle = color;
+                context.fill();
+            } else {
+                context.strokeStyle = color;
+                context.stroke();
+            }
             context.restore();
         }
 
